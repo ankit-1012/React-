55e4c01b3da3d61a3c58c9c872d6fdc0c2da6ac5
ankit <anshdubey1012@gmail.com>
2024-06-01 01:51:45 +0530

    building food app

--- index.js
+++ index.js
@@ -1,43 +1,62 @@
-// two types of dependices dev and normal 
 import React from "react";
 import ReactDom from "react-dom/client";
-//react element is not html element it only creates an object , 
-//when rendered it becomes html element that is using reactdom
 
-
-//this is jsx , html/xml like syntax but not html.
-
-const jsxheading = <p> this is jsx</p>
-//parcel(babel) transpiled b4 js engine gets it -> React element  -> html elemnt
-
-
-const root = ReactDom.createRoot(document.getElementById("root"));
-
-
-
-//React components 
-// class based components && functional components (FC are new based)
-
-
-// functional component normal javascript function which  return some jsx 
-
-const HeadingComp = () =>{
-    return <h1> hi from ankit</h1>
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://graphicsfamily.com/wp-content/uploads/edd/2021/06/Editable-Photoshop-Food-Logo-Design-PNG-Transparent-1536x1536.png"/>            
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+const RestaurantCard = () =>{
+    return (
+        //..img name rating cusiisne
+        <div className="res-card">  
+            <img 
+                className="res-logo" 
+                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e33e1d3ba7d6b2bb0d45e1001b731fcf"
+            />
+            <h3>Meghana Foods</h3>
+            <h4>Asian Indian Chinese</h4>
+            <h4>4.4 stars</h4>
+            <h4>30 mins</h4>
+        </div>
+    )
 }
-const num = 1000;
-// also called component composition (one into another)
-const HeadingComp2 = () => (
-    <div>
-        <h1> hi from anki 2</h1>
-        <p>{
-            //inside curly braces you can write any js code 
-            //u can also use functional component inside it 
-            //becuase at the end of the day they arre fucntions of javasctipt
-            jsxheading
-        }</p>
-
-        <HeadingComp/>
-    </div>
-);
-
-root.render(<HeadingComp2/>);
+const Body = () =>{
+    return (
+        <div className="body">
+            <div className="search">
+                Search
+            </div>
+            <div className="res-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                
+            </div>
+        </div>
+    )
+}
+const AppLayout = () =>{
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    );
+};
+const root = ReactDom.createRoot(document.getElementById("root"))
+root.render(<AppLayout/>)
